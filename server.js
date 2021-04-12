412bb91ad7fe74091a666d446159246586e59733
avalencia17 <47010418+avalencia17@users.noreply.github.com>
2021-04-11 17:50:40 -0700

    adolfo - node.js and socket.io stuff

---
+++ server.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//run when client connects
+io.on('connection', socket => {
+    //welcome a new client
+    socket.emit('message', 'Welcome to AJAchat!');
+    //show when a client connects
+    socket.broadcast.emit('message', 'New user has joined the chat!');
+    //show when cleint disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    });
+    //listen for chat message
+    socket.on('chatMessage', (msg) => {
+        io.emit('message', msg);
+    });
+});
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
