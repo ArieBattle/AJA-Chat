182f6bad600d83939b2b6fe83c23ee342d9702d7
avalencia17 <47010418+avalencia17@users.noreply.github.com>
2021-05-09 17:37:15 -0700

    adolfo-comments

--- server.js
+++ server.js
@@ -62,6 +62,8 @@ io.on('connection', socket => {
       }   
    });    
 });
+
+//to be able to run on a local machine
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
